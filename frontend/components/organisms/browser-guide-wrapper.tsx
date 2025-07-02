1e0c90abab671a0497edae6ece490c81f7880c04
JAEIK JEONG <wodlr1207@naver.com>
2025-07-03 00:32:25 +0900

    version 1.0.4

--- frontend/components/organisms/browser-guide-wrapper.tsx
+++ frontend/components/organisms/browser-guide-wrapper.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { BrowserGuideModal } from "@/components/molecules/browser-guide-modal";
+import { useBrowserGuide } from "@/hooks/useBrowserGuide";
 
 interface BrowserGuideWrapperProps {
   children: React.ReactNode;
 }
 
 export function BrowserGuideWrapper({ children }: BrowserGuideWrapperProps) {
+  const { showGuide, hideGuide } = useBrowserGuide();
+
   return (
     <>
       {children}
-      <BrowserGuideModal />
+      <BrowserGuideModal isOpen={showGuide} onOpenChange={() => hideGuide()} />
     </>
   );
 }
-
