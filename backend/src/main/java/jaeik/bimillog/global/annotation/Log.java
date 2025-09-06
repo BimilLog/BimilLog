a79ce839ad90a4595fbf0c4d3488bd9aa9905154
wodlr <wodlr1207@naver.com>
2025-09-06 21:47:06 +0900

    로깅 에스팩트 생성 (아직 미완성)

--- backend/src/main/java/jaeik/bimillog/global/annotation/Log.java
+++ backend/src/main/java/jaeik/bimillog/global/annotation/Log.java
@@ -1,4 +1,11 @@
 package jaeik.bimillog.global.annotation;
 
-public interface Log {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Log {
 }
