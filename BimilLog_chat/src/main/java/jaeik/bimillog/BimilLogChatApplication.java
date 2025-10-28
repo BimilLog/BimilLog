00c0d3c17add6e585c4c703bde8c1be51ac3b098
Jaeik <jaeik@example.com>
2025-10-28 17:32:47 +0900

    멀티모듈 변경

--- BimilLog_chat/src/main/java/jaeik/bimillog/BimilLogChatApplication.java
+++ BimilLog_chat/src/main/java/jaeik/bimillog/BimilLogChatApplication.java
@@ -1,4 +1,4 @@
-package jaeik.bimillog.chat;
+package jaeik.bimillog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- BimilLog_chat/src/test/java/jaeik/bimillog/BimilLogChatApplicationTests.java
+++ BimilLog_chat/src/test/java/jaeik/bimillog/BimilLogChatApplicationTests.java
@@ -1,4 +1,4 @@
-package jaeik.bimillog.chat;
+package jaeik.bimillog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
